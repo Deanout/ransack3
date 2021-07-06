687a8d1a2db037bcd694b54e3b4a4fdfe7d1d91b
Dean DeHart <DeHartDean@gmail.com>
2021-07-05 23:57:35 -0400

    Init commit

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  Machine.create(
+    name: Faker::Internet.url,
+    ip: Faker::Internet.ip_v4_address
+  )
+end
